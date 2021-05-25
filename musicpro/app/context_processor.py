32b242fc5159195abeec2954bfc690213cce78cd
Joaquinn <joaquin.armijojofre@gmail.com>
2021-05-24 21:02:41 -0400

    carrito sin errores

--- musicpro/app/context_processor.py
+++ musicpro/app/context_processor.py
@@ -1,6 +1,5 @@
 def importe_total_carro(request):
-    for key, value in request.session["carro"].items():
-        total=0
+    total=0
     if request.user.is_authenticated:
         for key, value in request.session["carro"].items():
             total=total+(float(value["precio"])*value["cantidad"])
